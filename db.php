2eca5c9a98010a72b305e075ac1b172d1ec2519f
Kelechukw <73535689+Kelechukw@users.noreply.github.com>
2020-12-04 02:38:59 +0000

    Update db.php

--- db.php
+++ db.php
@@ -15,26 +15,4 @@ $password = 'pulchritude';
         die("Connection Failed:" .mysqli_connect_error());
     }
 
-    //create Database
-    $sql="CREATE DATABASE IF NOT EXISTS $dbname";
-
-    if(mysqli_query($con,$sql)){
-        $con = mysqli_connect($servername,$username,$password,$dbname);
-$sql="
-CREATE TABLE IF NOT EXISTS playerss(
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Firstname VARCHAR(30) NOT NULL,
-    Lastname VARCHAR(30) NOT NULL,
-    Height VARCHAR(11) NOT NULL,
-    Location VARCHAR (50) NOT NULL );
-
-";
-if(mysqli_query($con,$sql)){
-    return $con;
-}else{
-    echo "Cannot Create Table";
-}
-    }else {
-        echo "Error while creating database". mysqli_error($con);
-    }
-}
+   
