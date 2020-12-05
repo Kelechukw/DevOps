c1b6b26e2687692b785c0e7a501291f57e822251
Kelechukw <73535689+Kelechukw@users.noreply.github.com>
2020-12-05 21:11:30 +0000

    Update db.php

--- db.php
+++ db.php
@@ -10,33 +10,7 @@ $password = 'pulchritude';
     //create connection
     $con = mysqli_connect("v2.csnql2pkuxse.eu-west-1.rds.amazonaws.com","kelly","pulchritude","volleyball");
 
-    //check connection
-    if(!$con){
-        die("Connection Failed:" .mysqli_connect_error());
-    }
-            $sql="CREATE DATABASE IF NOT EXISTS $dbname";
-    
-     if(mysqli_query($con,$sql)){
-        $con = mysqli_connect($servername,$username,$password,$dbname);
-$sql="
-CREATE TABLE IF NOT EXISTS playerss(
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Firstname VARCHAR(30) NOT NULL,
-    Lastname VARCHAR(30) NOT NULL,
-    Height VARCHAR(11) NOT NULL,
-    Location VARCHAR (50) NOT NULL );
-
-";
-if(mysqli_query($con,$sql)){
-    return $con;
-}else{
-    echo "Cannot Create Table";
-}
-    }else {
-        echo "Error while creating database". mysqli_error($con);
-    }
-}
-  
+}    
 
 
     
