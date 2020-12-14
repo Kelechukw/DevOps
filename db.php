294946baa7dabcffbf525ca06ed8753425036604
Kelechukw <73535689+Kelechukw@users.noreply.github.com>
2020-12-14 00:51:20 +0000

    Update db.php

--- db.php
+++ db.php
@@ -8,8 +8,8 @@ $password = 'pulchritude';
 
 
     //create connection
-    $link = new mysqli($_SERVER['v2.csnql2pkuxse.eu-west-1.rds.amazonaws.com'], $_SERVER['kelly'], $_SERVER['pulchritude'], $_SERVER['volleyball'], $_SERVER['3306']);
-    //$con = mysqli_connect("v2.csnql2pkuxse.eu-west-1.rds.amazonaws.com","kelly","pulchritude","volleyball");
+   
+ $con = mysqli_connect("v2.csnql2pkuxse.eu-west-1.rds.amazonaws.com","kelly","pulchritude","volleyball");
     
 }    
 
